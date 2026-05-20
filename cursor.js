95a3635440d5bda83d3fdbbaefb3a61db32f0457
Userunknown84 <adityasharma96469@gmail.cm>
2026-05-20 14:15:16 +0530

    commit

--- cursor.js
+++ cursor.js
@@ -4,26 +4,31 @@ const circles = document.querySelectorAll(".circle");
 let mouseX = 0;
 let mouseY = 0;
 
-const positions = Array.from(circles).map(() => ({ x: 0, y: 0 }));
+const positions = Array.from(circles).map(() => ({
+  x: 0,
+  y: 0
+}));
 
 window.addEventListener("mousemove", (e) => {
   mouseX = e.clientX;
   mouseY = e.clientY;
-
-  cursor.style.left = mouseX + "px";
-  cursor.style.top = mouseY + "px";
 });
 
 function animate() {
+
+  // Main cursor
+  cursor.style.transform = `translate(${mouseX}px, ${mouseY}px) translate(-50%, -50%)`;
+
   let x = mouseX;
   let y = mouseY;
 
   positions.forEach((pos, i) => {
-    pos.x += (x - pos.x) * 0.3;
-    pos.y += (y - pos.y) * 0.3;
 
-    circles[i].style.left = pos.x + "px";
-    circles[i].style.top = pos.y + "px";
+    pos.x += (x - pos.x) * 0.25;
+    pos.y += (y - pos.y) * 0.25;
+
+    circles[i].style.transform =
+      `translate(${pos.x}px, ${pos.y}px) translate(-50%, -50%)`;
 
     x = pos.x;
     y = pos.y;
