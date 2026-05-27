ca808a608273a00d4109709fe434b2e6ba5926ef
vidhimeshram <vidhimeshram7@gmail.com>
2026-05-27 17:41:43 +0530

    fix: resolve modal overlap with login and signup

--- cursor.js
+++ cursor.js
@@ -1,35 +1,35 @@
-const cursor = document.querySelector(".cursor");
-const circles = document.querySelectorAll(".circle");
+// const cursor = document.querySelector(".cursor");
+// const circles = document.querySelectorAll(".circle");
 
-let mouseX = 0;
-let mouseY = 0;
+// let mouseX = 0;
+// let mouseY = 0;
 
-const positions = Array.from(circles).map(() => ({ x: 0, y: 0 }));
+// const positions = Array.from(circles).map(() => ({ x: 0, y: 0 }));
 
-window.addEventListener("mousemove", (e) => {
-  mouseX = e.clientX;
-  mouseY = e.clientY;
+// window.addEventListener("mousemove", (e) => {
+//   mouseX = e.clientX;
+//   mouseY = e.clientY;
 
-  cursor.style.left = mouseX + "px";
-  cursor.style.top = mouseY + "px";
-});
+//   cursor.style.left = mouseX + "px";
+//   cursor.style.top = mouseY + "px";
+// });
 
-function animate() {
-  let x = mouseX;
-  let y = mouseY;
+// function animate() {
+//   let x = mouseX;
+//   let y = mouseY;
 
-  positions.forEach((pos, i) => {
-    pos.x += (x - pos.x) * 0.3;
-    pos.y += (y - pos.y) * 0.3;
+//   positions.forEach((pos, i) => {
+//     pos.x += (x - pos.x) * 0.3;
+//     pos.y += (y - pos.y) * 0.3;
 
-    circles[i].style.left = pos.x + "px";
-    circles[i].style.top = pos.y + "px";
+//     circles[i].style.left = pos.x + "px";
+//     circles[i].style.top = pos.y + "px";
 
-    x = pos.x;
-    y = pos.y;
-  });
+//     x = pos.x;
+//     y = pos.y;
+//   });
 
-  requestAnimationFrame(animate);
-}
+//   requestAnimationFrame(animate);
+// }
 
-animate();
+// animate();
