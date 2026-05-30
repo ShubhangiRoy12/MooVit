67c242b7cfd34175967d39be418b8613885b1540
Akhand <mishramudit117@gmail.com>
2026-05-30 17:22:36 +0530

    Fix custom cursor lag and improve animation smoothness

--- cursor.js
+++ cursor.js
@@ -9,21 +9,20 @@ const positions = Array.from(circles).map(() => ({ x: 0, y: 0 }));
 window.addEventListener("mousemove", (e) => {
   mouseX = e.clientX;
   mouseY = e.clientY;
-
-  cursor.style.left = mouseX + "px";
-  cursor.style.top = mouseY + "px";
 });
 
 function animate() {
+  // Main cursor
+  cursor.style.transform = `translate3d(${mouseX}px, ${mouseY}px, 0)`;
+
   let x = mouseX;
   let y = mouseY;
 
   positions.forEach((pos, i) => {
-    pos.x += (x - pos.x) * 0.3;
-    pos.y += (y - pos.y) * 0.3;
+    pos.x += (x - pos.x) * 0.18;
+    pos.y += (y - pos.y) * 0.18;
 
-    circles[i].style.left = pos.x + "px";
-    circles[i].style.top = pos.y + "px";
+    circles[i].style.transform = `translate3d(${pos.x}px, ${pos.y}px, 0)`;
 
     x = pos.x;
     y = pos.y;
